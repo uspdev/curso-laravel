b102e73b944d77960a1c042ef18ba356f58acafc
Ricardo Fernando dos Santos <st.ricardof@gmail.com>
2020-09-23 11:33:50 -0300

    Exercícios do treinamento com laravel MVC - Livros Ricardo Fernando dos Santos

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivroController;
+use App\Http\Controllers\LivroRicardoController;
 
 Route::get('/livros',[LivroController::class,'index']);
 
 Route::get('/livros/{isbn}',[LivroController::class,'show']);
+
+Route::get('/livros-ricardo',[LivroRicardoController::class,'index']);
+
+Route::get('/livros-ricardo/{isbn}',[LivroRicardoController::class,'show']);
