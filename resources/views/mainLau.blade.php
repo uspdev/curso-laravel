2953df5a9c782e48b7d5e172f2372bd56e95c69b
Laura Garcia <lauragarcia@usp.br>
2020-09-24 00:48:14 -0300

    Model, Controllers, Seeds e Faker

---
+++ resources/views/mainLau.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+      <container>
+        <card>
+         
+        @section('content') @show
+        </card>
+      </container> 
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivroController;
+use App\Http\Controllers\LivroLauController;
 
 Route::get('/livros',[LivroController::class,'index']);
 
 Route::get('/livros/{isbn}',[LivroController::class,'show']);
+
+// LivroLauraController
+
+Route::get('/livros_lau',[LivroLauController::class,'index']);
+
+Route::get('/livros_lau/{isbn}',[LivroLauController::class,'show']);
+
+
+
