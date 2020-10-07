0cd3ce8034a37d16e40b8bbbe87061f708655a9d
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-10-07 00:11:47 -0300

    parte 3 - validações

--- resources/views/livros/partials/form.blade.php
+++ resources/views/livros/partials/form.blade.php
@@ -2,8 +2,8 @@
 <script type="text/javascript" src="{ { asset('js/livro.js') } }"></script>
 @endsection
 
-Título: <input type="text" name="titulo" value="{{ $livro->titulo }}">
-Autor: <input type="text" name="autor" value="{{ $livro->autor }}">
-ISBN: <input type="text" name="isbn" value="{{ $livro->isbn }}">
+Título: <input type="text" name="titulo" value="{{ old('titulo',$livro->titulo) }}">
+Autor: <input type="text" name="autor" value="{{ old('autor',$livro->autor) }}">
+ISBN: <input type="text" name="isbn" value="{{ old('isbn',$livro->isbn) }}">
 <button type="submit">Enviar</button>
 
