d098dff1af7943fc1937237aa7d29eac70e2013b
Laura Garcia <lauragarcia@usp.br>
2020-10-07 22:58:52 -0300

    Validaçoes- parte 3 finalizada

---
+++ app/Http/Requests/LivrolauRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LivrolauRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'titulo' => 'required',
+            'autor'  => 'nullable',
+            'isbn'   => ['required','integer'],
+        ];
+        if ($this->method() == 'PATCH' || $this->method() == 'PUT'){
+            array_push($rules['isbn'], 'unique:livro_laus,isbn,' .$this->livrolau->id);
+        } else {
+            array_push($rules['isbn'], 'unique:livro_laus,isbn,');
+        }
+        return $rules;
+    }
+
+    public function messages()
+    {
+        return [
+            'isbn.unique' => 'Este ISBN está cadastrado para outro livro',
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'isbn' => preg_replace('/[^0-9]/', '', $this->isbn),
+        ]);
+    }
+}
