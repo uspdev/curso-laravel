2d3bb3b59d102afb30701abe1ac7819e0a6d1a21
Daniel Picon <dpicon@sc.usp.br>
2020-10-09 17:47:08 -0300

    Exercicio 1 - MVC - Feito

---
+++ database/seeders/LivroDanielSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\LivroDaniel;
+
+class LivroDanielSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $livroDaniel = [
+          'titulo'  => 'Meu primeiro Livro',
+            'autor' => 'Daniel Picon',
+            'isbn' => '1234567891234'
+        ];
+        LivroDaniel::create($livroDaniel);
+        LivroDaniel::factory(100)->create();
+        
+    }
+}
