c481c5d78b79cc831b9fecdbdabacf64c6a981bc
Marcelo Daudt <mdaudt@yahoo.com.br>
2020-10-09 15:20:47 -0300

    Realizado exercicios da semana 1 - MVC

---
+++ resources/views/livros_marcelo_daudt/partials/fields.blade.php
@@ -0,0 +1,7 @@
+<!-- ###### partials/fields.blade.php ###### -->
+
+<ul>
+  <li>{{ $livroMarceloDaudt->titulo ?? '' }}</li>
+  <li>{{ $livroMarceloDaudt->autor ?? '' }}</li>
+  <li>{{ $livroMarceloDaudt->isbn ?? '' }}</li>
+</ul>
