d32a51fa8cfb1e250abcf6a6eea19f53e9ce998f
Lucas Flóro <lucas@fearp.usp.br>
2020-10-09 16:31:21 -0300

    Criação de model, controller, migrations com artisan.
    Atualização de arquivos gerados com os códigos do exercício 01.

--- app/Http/Controllers/LivroLFloroController.php
+++ app/Http/Controllers/LivroLFloroController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LivroLFloroController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $livros = LivroLFloro::all();
@@ -15,12 +20,73 @@ class LivroLFloroController extends Controller
         ]);
     }
 
-    public function show($isbn)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LivroLFloro  $livroLFloro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LivroLFloro $livro)
     {
         $livro = LivroLFloro::where('isbn', $isbn)->first();
 
         return view('livroslfloro.show', [
             'livro' => $livro
         ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LivroLFloro  $livroLFloro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LivroLFloro $livroLFloro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LivroLFloro  $livroLFloro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LivroLFloro $livroLFloro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LivroLFloro  $livroLFloro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LivroLFloro $livroLFloro)
+    {
+        //
     }
 }
