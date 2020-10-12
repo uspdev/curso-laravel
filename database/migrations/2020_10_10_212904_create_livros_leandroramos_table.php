4e5e8b23eb73af005e19cd9d73341482d193ad19
Leandro Ramos <leandroembu@gmail.com>
2020-10-12 13:44:20 -0300

    Acerta nome da classe de migration
    Acertando o nome da classe, pois a migration não estava rodando com o outro nome

--- database/migrations/2020_10_10_212904_create_livros_leandroramos_table.php
+++ database/migrations/2020_10_10_212904_create_livros_leandroramos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLivrosLeandroRamosTable extends Migration
+class CreateLivroLeandroRamosTable extends Migration
 {
     /**
      * Run the migrations.
