01f93caf91a49f93b7204be1bb9baa4fd5bdae98
Daniel Picon <dpicon@sc.usp.br>
2020-10-13 17:29:40 -0300

    Exercício 2 - CRUD - OK

---
+++ database/factories/LivroDanielFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LivroDaniel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LivroDanielFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = LivroDaniel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'titulo' => $this->faker->sentence(3),
+            'isbn'   => $this->faker->ean13(),
+            'autor'  => $this->faker->name
+        ];
+    }
+}
