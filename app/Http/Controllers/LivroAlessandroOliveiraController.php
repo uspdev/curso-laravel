a7e8c22e89c8d873543318f55f9d6ba3b44fe23f
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2020-10-13 12:24:20 -0300

    controller crud

--- app/Http/Controllers/LivroAlessandroOliveiraController.php
+++ app/Http/Controllers/LivroAlessandroOliveiraController.php
@@ -23,14 +23,83 @@ class LivroAlessandroOliveiraController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param $livro
+     * @param LivroAlessandroOliveira $livro
      * @return \Illuminate\Http\Response
      */
-    public function show($livro)
+    public function show(LivroAlessandroOliveira $livro)
     {
-        $livro = LivroAlessandroOliveira::where('isbn', $livro)->first();
         return view('livros_alessandrooliveira.show', [
             'livro' => $livro,
         ]);
     }
+
+    /**
+     * Create resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('livros_alessandrooliveira.create',[
+            'livro' => new LivroAlessandroOliveira,
+        ]);
+    }
+    
+    /**
+     * Store resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $livro = new LivroAlessandroOliveira;
+        $livro->titulo = $request->titulo;
+        $livro->autor = $request->autor;
+        $livro->isbn = $request->isbn;
+        $livro->save();
+        return redirect("/livros_alessandrooliveira/{$livro->id}");
+    }
+
+    /**
+     * Edit the specified resource.
+     *
+     * @param LivroAlessandroOliveira $livro
+     * @return \Illuminate\Http\Response
+     */    
+    public function edit(LivroAlessandroOliveira $livro)
+    {
+        return view('livros_alessandrooliveira.edit',[
+            'livro' => $livro
+        ]);
+    }
+    
+    /**
+     * Update the specified resource.
+     *
+     * @param Request $request
+     * @param LivroAlessandroOliveira $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LivroAlessandroOliveira $livro)
+    {
+        $livro->titulo = $request->titulo;
+        $livro->autor = $request->autor;
+        $livro->isbn = $request->isbn;
+        $livro->save();
+        return redirect("/livros_alessandrooliveira/{$livro->id}");
+    }
+    
+    /**
+     * Destroy the specified resource.
+     *
+     * @param LivroAlessandroOliveira $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LivroAlessandroOliveira $livro)
+    {
+        $livro->delete();
+        return redirect('/livros_alessandrooliveira');
+    }
+
 }
