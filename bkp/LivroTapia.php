c458333ac2a24279c13ee817e4471d5da8642ce8
Luciano Tapia <lrtapia@usp.br>
2020-10-15 10:25:19 -0300

    Deletando Arquivos Aula 1

--- bkp/LivroTapia.php
+++ bkp/LivroTapia.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class LivroTapia extends Model
 {
     use HasFactory;
+    // Necessário para quando não seguir o padrão de nomeação, ou seja,
+    // nome da tabela não estiver no plural.
+    //protected $table = "nome_da_tabela";
 }
