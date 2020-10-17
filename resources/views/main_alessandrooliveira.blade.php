1d10e67689a67e43b22f195b4253d064e8ec10d9
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2020-10-17 13:22:32 -0300

    add input mask isbn

--- resources/views/main_alessandrooliveira.blade.php
+++ resources/views/main_alessandrooliveira.blade.php
@@ -1,2 +1,5 @@
 @extends('laravel-usp-theme::master')
 
+@section('javascripts_bottom') 
+    <script type="text/javascript" src="{{ asset('js/livro_alessandrooliveira.js') }}"></script>
+@endsection
