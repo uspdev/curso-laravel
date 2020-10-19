7ca55a2bb5549fdc8437cac464e1368227b81e3e
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2020-10-19 10:56:58 -0300

    issue 3.5 form request

--- app/Models/LivroAlessandroOliveira.php
+++ app/Models/LivroAlessandroOliveira.php
@@ -9,4 +9,5 @@ class LivroAlessandroOliveira extends Model
 {
     use HasFactory;
     protected $table = 'livros_alessandrooliveira';
+    protected $guarded = ['id'];
 }
--- resources/views/livros_alessandrooliveira/partials/form.blade.php
+++ resources/views/livros_alessandrooliveira/partials/form.blade.php
@@ -1,5 +1,5 @@
 <!-- ###### partials/form.blade.php ###### -->
-Título: <input type="text" name="titulo" value="{{ $livro->titulo }}">
-Autor: <input type="text" name="autor" value="{{ $livro->autor }}">
-ISBN: <input type="text" name="isbn" class="isbn" value="{{ $livro->isbn }}">
+Título: <input type="text" name="titulo" value="{{ old('titulo', $livro->titulo) }}">
+Autor: <input type="text" name="autor" value="{{ old('autor', $livro->autor) }}">
+ISBN: <input type="text" name="isbn" class="isbn" value="{{ old('isbn', $livro->isbn) }}">
 <button type="submit">Enviar</button>
