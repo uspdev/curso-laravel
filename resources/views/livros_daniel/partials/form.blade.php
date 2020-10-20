02a68f6953e82585305fb867923b821a2eac4653
Daniel Picon <dpicon@sc.usp.br>
2020-10-20 09:56:15 -0300

    Aplicanto USP-Laravel-Theme

--- resources/views/livros_daniel/partials/form.blade.php
+++ resources/views/livros_daniel/partials/form.blade.php
@@ -1,5 +1,15 @@
-Título: <input type='text' name="titulo" value="{{$livro->titulo}}">
+ 
+<label for="titulo" class="ml-5">Titulo:</label>
+<input type="text" class="form-control ml-5" name="titulo" value="{{$livro->titulo}}">
+<label for="autor" class="ml-5">Autor:</label>
+<input type="text" class="form-control ml-5" name="autor" value="{{$livro->autor}}">
+<label for="isbn" class="ml-5">ISBN:</label>
+<input type="text" class="form-control ml-5" name="isbn" value="{{$livro->isbn}}">
+
+<button type="submit" class="btn btn-primary ml-5">Enviar</button>
+
+<!--Título: <input type='text' name="titulo" >
 Autor: <input type='text' name="autor" value="{{$livro->autor}}">
 ISBN: <input type='text' name="isbn" value="{{$livro->isbn}}">
 
-<button type="submit">Enviar</button>
+<button type="submit">Enviar</button>-->
--- resources/views/main_daniel.blade.php
+++ resources/views/main_daniel.blade.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel no contexto USP</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        @section('content') @show
-    </body>
-</html>
+@extends('laravel-usp-theme::master')
