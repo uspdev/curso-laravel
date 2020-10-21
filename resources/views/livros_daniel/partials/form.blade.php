dd7673e164f200627c545599136966b888e761f4
Daniel Picon <dpicon@sc.usp.br>
2020-10-21 10:27:24 -0300

    Exercício 3 - Validação - OK

--- resources/views/livros_daniel/partials/form.blade.php
+++ resources/views/livros_daniel/partials/form.blade.php
@@ -1,15 +1,9 @@
  
 <label for="titulo" class="ml-5">Titulo:</label>
-<input type="text" class="form-control ml-5" name="titulo" value="{{$livro->titulo}}">
+<input type="text" class="form-control ml-5" name="titulo" value="{{old('titulo', $livro->titulo)}}">
 <label for="autor" class="ml-5">Autor:</label>
-<input type="text" class="form-control ml-5" name="autor" value="{{$livro->autor}}">
+<input type="text" class="form-control ml-5" name="autor" value="{{old('autor', $livro->autor)}}">
 <label for="isbn" class="ml-5">ISBN:</label>
-<input type="text" class="form-control ml-5" name="isbn" value="{{$livro->isbn}}">
+<input type="text" class="form-control ml-5" name="isbn" value="{{old('isbn', $livro->isbn)}}">
 
 <button type="submit" class="btn btn-primary ml-5">Enviar</button>
-
-<!--Título: <input type='text' name="titulo" >
-Autor: <input type='text' name="autor" value="{{$livro->autor}}">
-ISBN: <input type='text' name="isbn" value="{{$livro->isbn}}">
-
-<button type="submit">Enviar</button>-->
