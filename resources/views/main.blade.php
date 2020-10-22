9d44cf1ebed78d95e0c83be2c344478d66fd6873
Edmar Martineli <edmar@usp.br>
2020-10-22 16:16:18 -0300

    Arrumando rotas

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>@section('title') Exemplo @show</title>
-    </head>
-    <body>
-       @section('content') @show
-    </body>
-</html>
+@extends('laravel-usp-theme::master')
